c92f08efe1879684d771869b47c55dec79e926f4
ChaitanyaBaweja <chaitanya.baweja1@gmail.com>
2016-07-12 16:16:38 +0530

    RGB LED test program

---
+++ arduino/RGBLED.ino
@@ -0,0 +1,30 @@
+/* < Program to test RGB LED Strip
+ 
+ * A RGB LED was added to an arduino UNO with the sensor to indicate level of pollution >
+ 
+ * Copyright (C) <2016>  <Chaitanya Baweja> <http://www.gnu.org/licenses/>
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+*/
+void setup() {
+  // put your setup code here, to run once:
+pinMode(10, OUTPUT);
+pinMode(9, OUTPUT);
+pinMode(8, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(10, HIGH);
+delay(3000);
+digitalWrite(10, LOW);
+digitalWrite(9, HIGH);
+delay(3000);
+digitalWrite(9, LOW);
+digitalWrite(8, HIGH);
+delay(3000);
+digitalWrite(8, LOW);
+}
