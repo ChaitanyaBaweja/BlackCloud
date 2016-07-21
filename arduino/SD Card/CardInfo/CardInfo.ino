5e2a6fade9f68b083b4f541adce3e1a16f16038e
ChaitanyaBaweja <chaitanya.baweja1@gmail.com>
2016-07-21 17:03:46 +0530

    added license info

--- arduino/SD Card/CardInfo/CardInfo.ino
+++ arduino/SD Card/CardInfo/CardInfo.ino
@@ -9,6 +9,13 @@
  ** UNO:  MOSI - pin 11, MISO - pin 12, CLK - pin 13, CS - pin 4 (CS pin can be changed)
   and pin #10 (SS) must be an output
  */
+ /* Copyright (C) <2016>  <Chaitanya Baweja> <http://www.gnu.org/licenses/>
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+*/
  // include the SD library:
 #include <SPI.h>
 #include <SD.h>
